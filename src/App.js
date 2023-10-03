3472adf65c2f4a5ba2ac63b9137322d88af27c29
Islambek201632838 <islambek.sadykov@nu.edu.kz>
2023-10-03 14:15:46 +0600

    Add project files

--- src/App.js
+++ src/App.js
@@ -7,18 +7,42 @@ function App() {
 
   const addTask = () => {
     if (task.trim() !== '') {
-      setTasks([...tasks, task]);
+      setTasks([...tasks, { task: task, status: '', backgroundColor: 'white' }]);
       setTask('');
     }
   };
 
   const removeTask = (index) => {
-    const updatedTasks = tasks.filter((_, i) => i !== index);
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleStatusChange = (index, selectedStatus) => {
+    const updatedTasks = tasks.map((task, i) => {
+      if (i === index) {
+        let backgroundColor;
+        switch (selectedStatus) {
+          case 'done':
+            backgroundColor = 'lightgreen';
+            break;
+          case 'wasnt done':
+            backgroundColor = 'lightcoral';
+            break;
+          default:
+            backgroundColor = 'white';
+            break;
+        }
+        return { ...task, status: selectedStatus, backgroundColor };
+      } else {
+        return task;
+      }
+    });
     setTasks(updatedTasks);
   };
 
   return (
-    <div className="container mt-5" style={{width:'1000px'}}>
+    <div className="container mt-5" style={{ maxWidth: '1000px' }}>
       <h1 className="text-center mb-4">To-Do List</h1>
       <div className="input-group mb-3">
         <input
@@ -31,24 +55,40 @@ function App() {
         <button
           onClick={addTask}
           className="btn btn-primary"
-          style={{ fontSize: '30px' }}
+          style={{
+            fontSize: '30px',
+            backgroundColor: task.trim() === '' ? 'grey' : ''
+          }}
+          disabled={task.trim() === ''}
         >
           Add Task
         </button>
       </div>
       <ul className="list-group">
-        {tasks.map((task, index) => (
+        {tasks.map((taskObj, index) => (
           <li
             key={index}
             className="list-group-item d-flex justify-content-between align-items-center"
+            style={{ backgroundColor: taskObj.backgroundColor }}
           >
-            {task}
-            <button
-              className="btn btn-danger btn-sm"
-              onClick={() => removeTask(index)}
-            >
-              Remove
-            </button>
+            {taskObj.task}
+            <div className="d-flex align-items-center">
+              <select
+                value={taskObj.status}
+                onChange={(e) => handleStatusChange(index, e.target.value)}
+                className="form-select me-3"
+              >
+                <option value="">Выбрать статус</option>
+                <option value="done">Сделано</option>
+                <option value="wasnt done">Не сделано</option>
+              </select>
+              <button
+                className="btn btn-danger btn-sm"
+                onClick={() => removeTask(index)}
+              >
+                Remove
+              </button>
+            </div>
           </li>
         ))}
       </ul>
